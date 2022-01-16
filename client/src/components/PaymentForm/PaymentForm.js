44edf1b092c053ba4e813439d476f3fdf5da3b2d
Ahmed Attawia <ahmed.attawia@gmail.com>
2022-01-16 23:07:21 +0100

    payment validation

--- client/src/components/PaymentForm/PaymentForm.js
+++ client/src/components/PaymentForm/PaymentForm.js
@@ -26,11 +26,13 @@ const PaymentForm = () => {
 
     const [paymentInfo,setPaymentInfo] = react.useState({
         cardNo : "",
-        name : "",
+        custName : "",
         cvv : "",
         expiryMonth : "",
         expiryYear : "",
     });
+
+    const [message,setMessage] = react.useState("");
 
     const [successFlag,setSuccessFlag] = react.useState(false);
 
@@ -42,18 +44,36 @@ const PaymentForm = () => {
     }
 
     const Submit = async (e) =>{
+        message.setMessage("");
         e.preventDefault();
+        if(paymentInfo.cardNo.length != 16){
+            message += "Card number invalid \n";
+        }
+        const namere = /[a-zA-Z]+\s[a-zA-Z]+[\s[a-zA-Z]*/;
+        if(!(namere.test(paymentInfo.custName))){
+            setMessage(message += "Name invalid \n");
+        }
+        if(paymentInfo.cvv.length != 3){
+            setMessage(message += "CVV invalid \n");
+        }
+        if(paymentInfo.expiryMonth < 1 || paymentInfo.expiryMonth >12){
+            setMessage(message += "Month should be between 1 and 12");
+        }
+        if(paymentInfo.expiryYear < 22){
+            setMessage(message += "Expiry Year cannot be in the past");
+        }
+        if(message == ""){
         const res = await makePayment({amount: amount*100});
         if(res.status == 200){
             setSuccessFlag(true);
         }
-        
         await timeout(1000);
         history.push({
             pathname: '/allreservations/selectedflight',
             state:{reservation,type,flight}
           });
           window.location.reload();
+        }
     };
 
     const onChangeCard = (e) => {
@@ -96,7 +116,8 @@ const PaymentForm = () => {
                 </button>
             </Link>
             <h1 variant="h5">Confirm Payment of {amount}€</h1>
-            <TextField  name="Cardholder's Name" label="Cardholder's Name"   variant="outlined"  value={paymentInfo.name} onChange={(e) => setPaymentInfo({...paymentInfo, name : e.target.value})}/><br/><br/>
+            <h1 variant="h5">{message}</h1>
+            <TextField  name="Cardholder's Name" label="Cardholder's Name"   variant="outlined"  value={paymentInfo.name} onChange={(e) => setPaymentInfo({...paymentInfo, custName : e.target.value})}/><br/><br/>
             <TextField  name="Card Number"  variant="outlined" label="Card Number"  value={paymentInfo.cardNo} onChange={onChangeCard}/><br/>
             <TextField  name="CVV" type="password"  label="CVV" margin="dense" style={{width: 70}}  variant="outlined"  value={paymentInfo.cvv} onChange={onChangeCvv}/> 
             <TextField  name="Expiry Date"  margin="dense" style={{width: 50}} variant="outlined" label="MM"  value={paymentInfo.expiryMonth} onChange={onChangeM}/>
